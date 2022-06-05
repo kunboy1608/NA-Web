b8eaa368be24c048faf77007e227a27b203288fa
hoangdp <kunboy1608@gmail.com>
2022-06-05 08:56:23 +0700

    Add Login, HomePage

---
+++ scripts/scripts1.js
@@ -0,0 +1,33 @@
+function abc(a) {
+    var myFrame = document.getElementById("mainframe"); 
+    switch (a) {
+        case 0:
+            myFrame.scr = "../manage/student.html";
+            print (aaa);
+            break;
+        case 1:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 2:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 3:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 4:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 5:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 6:
+            myFrame.scr = "../manage/student.html";
+            break;
+        case 7:
+            myFrame.scr = "../manage/student.html";
+            break;
+        default:
+            break;
+    }
+
+}
